eb64ca1d9260687a5055b1e4b6b2f280799d471a
TheKrenker <dennis.overman@home.nl>
2022-03-28 09:36:56 +0200

    Fix: Layout gemaakt + head.php gemaakt
    Fix: #16
    Fix: #17

---
+++ Website/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Lorem Ipsum</title>
+        <?php @require 'layout/head.php' ?>
+    </head>
+
+    <body>
+        <?php @require 'layout/header.php' ?>
+
+        <main>
+
+        </main>
+
+        <?php @require 'layout/footer.php' ?>
+    </body>
+</html>
