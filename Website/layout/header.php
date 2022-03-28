76d59b08d863773e3c77a609ed55071ec5d17cbb
TheKrenker <dennis.overman@home.nl>
2022-03-28 09:58:57 +0200

    Feat: WIP Navbar

--- Website/layout/header.php
+++ Website/layout/header.php
@@ -1,3 +1,18 @@
 <header>
-    
+    <nav>
+        <div class="wrapper">
+            <div class="navbar">
+                <div class="navleft">
+                    <img src="../img/logo.png" alt="SuperCoaster Logo">
+                </div>
+
+                <div class="navright">
+                    <a href="index.php" class="active">Home</a>
+                    <a href="">Constructie</a>
+                    <a href="">Treintjes</a>
+                    <a href="contact.php">Contact</a>
+                </div>
+            </div>
+        </div>
+    </nav>
 </header>
