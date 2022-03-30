b77bc951aa0937a84d934917705458cd9d20a99e
Pluk Zwaal <Plukky.zwaal@gmail.com>
2022-03-30 12:08:05 +0200

    footer quick save

--- Website/layout/footer.php
+++ Website/layout/footer.php
@@ -1,3 +1,17 @@
 <footer>
-            
+    <div class="wrapper">
+        <p>&copy; SuoerCoasterNv</p>
+        <div class="pagesFooter">
+            <a href="">Home</a>
+            <a href="">Constructie</a>
+            <a href="">Treintjes</a>
+            <a href="">Contact</a>
+        </div>
+        <div class="socialsFooter">
+            <a id="fa-facebook" href="" class="fa fa-facebook"></a>
+            <a id="fa-instagram" href="" class="fa fa-instagram"></a>
+            <a id="fa-twitter" href="" class="fa fa-twitter"></a>
+            <a id="fa-youtube" href="" class="fa fa-youtube"></a>
+        </div>
+    </div>                
 </footer>
