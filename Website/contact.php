9880f909fb4bee72d591e821952a269a07932411
TheKrenker <dennis.overman@home.nl>
2022-04-06 11:35:21 +0200

    Feat: Busy with contact.php

---
+++ Website/contact.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Lorem Ipsum</title>
+        <?php require_once 'layout/head.php' ?>
+    </head>
+
+    <body>
+        <?php require_once 'layout/header.php' ?>
+
+        <main>
+            <div class="wrapper">
+                <form action="">
+                    <div class="form-group">
+                        <label for="fname">Voornaam:</label>
+                        <input id="fname" name="fname" type="text">
+                    </div>               
+                </form>
+            </div>
+        </main>
+        <footer>
+            <?php require_once 'layout/footer.php' ?>
+        </footer>
+    </body>
+</html>
