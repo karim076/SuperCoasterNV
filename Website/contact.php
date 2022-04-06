4c918613c87bed189f1402d43186e00b1142859e
TheKrenker <dennis.overman@home.nl>
2022-04-06 11:41:29 +0200

    Form making

--- Website/contact.php
+++ Website/contact.php
@@ -11,10 +11,25 @@
         <main>
             <div class="wrapper">
                 <form action="">
-                    <div class="form-group">
+                <div class="form-group">
                         <label for="fname">Voornaam:</label>
                         <input id="fname" name="fname" type="text">
-                    </div>               
+                    </div>
+
+                    <div class="form-group">
+                        <label for="lname">Achternaam:</label>
+                        <input id="lname" name="lname" type="text">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input id="email" name="email" type="text">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="message">Message:</label>
+                        <textarea name="message" id="message" cols="30" rows="2"></textarea>
+                    </div>
                 </form>
             </div>
         </main>
