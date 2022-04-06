bc8232660fea1310bb3723b1f37a2e268bb470a4
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-06 11:48:38 +0200

    Added contact form

--- Website/contact.php
+++ Website/contact.php
@@ -1,40 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>Lorem Ipsum</title>
-        <?php require_once 'layout/head.php' ?>
-    </head>
+<head>
+    <title>Lorem Ipsum</title>
+    <?php require_once 'layout/head.php' ?>
+</head>
 
-    <body>
-        <?php require_once 'layout/header.php' ?>
+<body>
+    <?php require_once 'layout/header.php' ?>
 
-        <main>
-            <div class="wrapper">
-                <form action="">
-                <div class="form-group">
-                        <label for="fname">Voornaam:</label>
-                        <input id="fname" name="fname" type="text">
-                    </div>
+    <main>
+        <div class="wrapper">
+            <form class="form-style-9">
+                <ul>
+                    <li>
+                        <input type="text" name="field1" class="field-style field-split align-left" placeholder="Name" />
+                        <input type="email" name="field2" class="field-style field-split align-right" placeholder="Email" />
 
-                    <div class="form-group">
-                        <label for="lname">Achternaam:</label>
-                        <input id="lname" name="lname" type="text">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input id="email" name="email" type="text">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="message">Message:</label>
-                        <textarea name="message" id="message" cols="30" rows="2"></textarea>
-                    </div>
-                </form>
-            </div>
-        </main>
-        <footer>
-            <?php require_once 'layout/footer.php' ?>
-        </footer>
-    </body>
+                    </li>
+                    <li>
+                        <input type="text" name="field3" class="field-style field-split align-left" placeholder="Phone" />
+                        <input type="url" name="field4" class="field-style field-split align-right" placeholder="Website" />
+                    </li>
+                    <li>
+                        <input type="text" name="field3" class="field-style field-full align-none" placeholder="Subject" />
+                    </li>
+                    <li>
+                        <textarea name="field5" class="field-style" placeholder="Message"></textarea>
+                    </li>
+                    <li>
+                        <input type="submit" value="Send Message" />
+                    </li>
+                </ul>
+            </form>
+        </div>
+    </main>
+    <footer>
+        <?php require_once 'layout/footer.php' ?>
+    </footer>
+</body>
 </html>
