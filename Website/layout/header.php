a0c3c19b35d89d478fad38df52baf4e001538823
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-11 19:59:21 +0200

    Added links to page

--- Website/layout/header.php
+++ Website/layout/header.php
@@ -1,14 +1,14 @@
 <header>
     <div class="firstLeft">
-        <img src="http://localhost/Tweede%20Periode/SuperCoasterNV/Website/img/logo.png" alt="logo">
+        <img src="img/logo.png" alt="logo">
         <h2>SuperCoasterNV</h2>
     </div>
     <nav>
         <ul class="nav__links">
             <li><a href="#">Constructie</a></li>
             <li><a href="#">Achtbaan</a></li>
-            <li><a href="#">Contact</a></li>
+            <li><a href="../contact.php">Contact</a></li>
        </ul>
     </nav>
     <a class="cta" href="#">Home</a>
-</header>
+</header>   
