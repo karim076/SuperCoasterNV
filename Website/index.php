c1d978020d394312cf9d759bfb2a87a4ecd2f7c7
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-11 21:45:20 +0200

    Added new image and text above inprogress not done yet

--- Website/index.php
+++ Website/index.php
@@ -9,15 +9,16 @@
         <?php require_once 'layout/header.php' ?>
 
         <main>
-            <div class="top">
-                <div class="wrapper">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Ea culpa aspeess</p>               
-                </div>
+        <div class="containers">
+            <img src="img/frontpage.jpg" alt="Snow">
+            <div class="centered">
+                <h6>SuperCoaster</h6>
+                <p><span>"</span>Hier kun je het vinden<span>"</span></p>
             </div>
+        </div>
             <div class="indexText">
                 <div class="wrapper">
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing, elit. A neque consequuntur illum quidem ad molestias earum, magnam amet eveniet ipsum, veniam nulla sit maiores!</p>
-                    <img src="img/logo.png">
                 </div>
             </div>
         </main>
