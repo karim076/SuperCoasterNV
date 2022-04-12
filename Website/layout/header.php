22ba26aafdf38ce52cfda8a548b33f88cfcfc427
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-12 23:11:40 +0200

    Fixed contact page not going to the right path

--- Website/layout/header.php
+++ Website/layout/header.php
@@ -7,7 +7,7 @@
         <ul class="nav__links">
             <li><a href="#">Constructie</a></li>
             <li><a href="#">Achtbaan</a></li>
-            <li><a href="../contact.php">Contact</a></li>
+            <li><a href="contact.php">Contact</a></li>
        </ul>
     </nav>
     <a class="cta" href="#">Home</a>
