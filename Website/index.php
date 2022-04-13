5efd51ac81f70a05951f3dd874baa9ce03cbe770
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-13 15:07:14 +0200

    Added plain text and img to box

--- Website/index.php
+++ Website/index.php
@@ -19,7 +19,13 @@
         <div class="wrapping">
             <div class="row">
                 <div class="box">
-                    
+                    <div class="top">
+                        <img src="img/service.png" alt="service icon">
+                        <h3>Service</h3>
+                    </div>
+                    <div class="text">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta error vel, numquam nostrum neque quasi deserunt laborum quisquam consectetur possimus quae sequi iusto! Fuga similique harum aliquid nemo ipsa enim eveniet blanditiis eligendi explicabo eos maxime laborum, illum a omnis culpa voluptatum tenetur accusamus, recusandae cum repellendus? Nostrum rerum, fugiat sit aperiam placeat ad ipsam vel expedita. Explicabo porro, nihil fugiat possimus error aperiam reiciendis consequuntur quae obcaecati aspernatur, soluta eius dolores, modi alias distinctio?</p>
+                    </div>
                 </div>
                 <div class="box">
                     
