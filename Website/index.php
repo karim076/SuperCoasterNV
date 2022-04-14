0739b7ec005bc654ee0fe9ea721936d99c99c6c0
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-14 10:31:26 +0200

    Added all text+img

--- Website/index.php
+++ Website/index.php
@@ -18,32 +18,66 @@
         </div>
         <div class="wrapping">
             <div class="row">
-                <img src="img/2.jpg"
                 <div class="box">
                     <div class="top">
                         <img src="img/service.png" alt="service icon">
                         <h3>Service</h3>
                     </div>
                     <div class="text">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta error vel, numquam nostrum neque quasi deserunt laborum quisquam consectetur possimus quae sequi iusto! Fuga similique harum aliquid nemo ipsa enim eveniet blanditiis eligendi explicabo eos maxime laborum, illum a omnis culpa voluptatum tenetur accusamus, recusandae cum repellendus? Nostrum rerum, fugiat sit aperiam placeat ad ipsam vel expedita. Explicabo porro, nihil fugiat possimus error aperiam reiciendis consequuntur quae obcaecati aspernatur, soluta eius dolores, modi alias distinctio?</p>
+                        <p>Onze service voor onze klanten, door dat wij onze nieuwe producten verkopen, plaatsen we daar ook garantie op.<br>Onze garantie die wij aanbieden is heel breed van technische problem tot pysieke problemen wij kunnen het fixen<br>Wij doen zo veel mogelijk om onze klanten te vreden houden, dit hebben we ook behaald bij al onze andere klanten.</p>
                     </div>
                 </div>
                 <div class="box">
-                    
+                    <div class="top">
+                        <img src="img/rollercoaster.png" alt="achtbaan icon">
+                        <h3>Achtbanen</h3>
+                    </div>
+                    <div class="text">
+                        <p>Wat voor soort Achtbanen hebben wij aan te bieden:<br>-Python<br>-Joris en de draak<br>-Pegasus<br>Wij zijn natuurlijk altijd nog bezig om nieuwe en een te gekke achtbaan te inoveren, waar onze klanten "wauw" kunnen voor zeggen</p>
+                    </div>
                 </div>
                 <div class="box">
-                    
+                    <div class="top">
+                        <img src="img/info.png" alt="info icon">
+                        <h3>Over ons</h3>
+                    </div>
+                    <div class="text">
+                        <p>Wie zijn wij?<br>Wij zijn een bedrijf die opgezet is in 1989, wij creëerde toen de tijd railen voor het achtbaan,<br>Maar na 2001 dachten we dat we ook achtbanen gaan bouwen, en dat was een succesvolle missie die wij hebben samen met onze klanten hebben behaald.<br>Wij hebben klanten van het beroemdste pretparken zoals, walibi, Efteling en Drievliet.</p>
+                    </div>
                 </div>
             </div>
             <div class="row">
                 <div class="box">
-                    
+                    <div class="top">
+                        <img src="img/quality.png" alt="kwaliteit icon">
+                        <h3>Kwaliteit</h3>
+                    </div>
+                    <div class="text">
+                        <p>Wij zijn een bedrijf die kwaliteit producten levert, hiermee bedoelen we dat onze product met hoge kwaliteit geproduceerd wordt.<br>Heeft u intresse hoe onze achtbanen worden gecreëedt bekijk dan Karretjes pagina, daar vertellen we meer over het bouw materialen en hoe dat tot een kwaliteit product wordt omgezet.<br></p>
+                    </div>
                 </div>
                 <div class="box">
-                    
+                    <div class="top">
+                        <img src="img/queing.png" alt="wachtrij icon">
+                        <h3>Wachtrij</h3>
+                    </div>
+                    <div class="text">
+                        <p>Bij onze achtbanen die wij leveren zitten ook nadelen, maar daar hebben we een oplossing voor bedacht.<br>Heeft u ooit last gehad van een lange wachtrij,<br> zo ja dan prijzen we u onze product aan.<br>Wij hebben de zit plaatsen zo gemaakt dat het veilig is en genoeg plek heeft, door meer plek te creëeren dit lost al 50% van het lange rijen.</p>
+                    </div>
                 </div>
                 <div class="box">
-                    
+                    <div class="top">
+                        <img src="img/feedback.png" alt="Beoordeling icon">
+                        <h3>Onze klanten</h3>
+                    </div>
+                    <div class="text">
+                        <p>Wij produceren hoge kwaliteiten aan onze klanten, de klanten die wij blij hebben gemaakt zijn</p>
+                    </div>
+                    <div class="logos">
+                        <a href="https://www.walibi.nl/nl"> <img src="img/walibi.png" alt="walibie logo"></a>
+                        <a href="https://www.drievliet.nl/"><img src="img/drievliet.png" alt="toverland logo"></a>
+                        <a href="https://www.efteling.com/nl"><img src="img/efteling.png" alt="Efteling logo"></a>
+                    </div>
                 </div>
             </div>
         </div>
