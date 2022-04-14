b7d8dc24e43f5b8daedfcf589fc1c6897751fbc4
Pluk Zwaal <Plukky.zwaal@gmail.com>
2022-04-14 10:57:56 +0200

    Text change on constrcution page

--- Website/constructie.php
+++ Website/constructie.php
@@ -17,7 +17,7 @@
             </div>
             <div class="textCon">
                 <div class="wrapper">
-                    <p>Het eerste USP die wij voor deze achtbaan hebben ontwikkeld is een nieuwe metaal messing die nog nooit eerder in deze industrie gebruikt is. Door een menging te maken van 69% staal, 20% nikkel, en 11% koper hebben een nieuwe messing ontwikkeld die wij nu MagnaAlloy noemen. Met deze nieuwe MagnaAlloy maken wij een aantal onderdelen die de meeste stress ondergaan in de achtbaan, waardoor deze nog meer G-krachten kunnen weerstaan, en langer meegaan dan andere stalen. We hebben enorm geprobeerd de kosten zo laag mogelijk te houden, dat de onderdelen gemiddeld slechts 8% duurder uitvallen dan andere soortgelijke onderdelen, zonder kwaliteitsverlies. Onze tests bewijzen ook dat de onderdelen tot 25% langer meegaan, dus dit zou uiteindelijk ook in kosten moeten besparen. Ook zorgt dit ervoor dat een aantal designs voor achtbanen die eerder niet mogelijk waren nu wel mogelijk zijn.</p>
+                    <p>De constructie van de achtbanen hebben een speciaal soort messing. Dit is 69% staal, 20% nikkel en 11% koper. Wij moeten dit MagnaAlloy. Door deze messing gaat de atractie nog sneller dan alle andere achtbanen en gaat de atractie tot wel 25% langer mee. Dit betekent dus dat de kosten goedkoper zijn. Door dit metaal kunnen we speciale vormen maken die we daarvoor niet konden maken.</p>
                     <img src="img/6.jpg">                
                 </div>              
             </div>
