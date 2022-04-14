1feccea28dae6fb78fff01beff70e4b07bc258d7
Pluk Zwaal <Plukky.zwaal@gmail.com>
2022-04-14 11:12:19 +0200

    update spelfout constructie

--- Website/constructie.php
+++ Website/constructie.php
@@ -17,7 +17,7 @@
             </div>
             <div class="textCon">
                 <div class="wrapper">
-                    <p>De constructie van de achtbanen hebben een speciaal soort messing. Dit is 69% staal, 20% nikkel en 11% koper. Wij moeten dit MagnaAlloy. Door deze messing gaat de atractie nog sneller dan alle andere achtbanen en gaat de atractie tot wel 25% langer mee. Dit betekent dus dat de kosten goedkoper zijn. Door dit metaal kunnen we speciale vormen maken die we daarvoor niet konden maken.</p>
+                    <p>De constructie van de achtbanen hebben een speciaal soort messing. Dit is 69% staal, 20% nikkel en 11% koper. Wij noemen dit MagnaAlloy. Door deze messing gaat de atractie nog sneller dan alle andere achtbanen en gaat de atractie tot wel 25% langer mee. Dit betekent dus dat de kosten goedkoper zijn. Door dit metaal kunnen we speciale vormen maken die we daarvoor niet konden maken.</p>
                     <img src="img/6.jpg">                
                 </div>              
             </div>
