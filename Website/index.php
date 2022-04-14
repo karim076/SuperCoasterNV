6c4942f430c1af8abbcf35975f37afa4af5dddc5
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-14 10:42:46 +0200

    Added p start

--- Website/index.php
+++ Website/index.php
@@ -34,7 +34,7 @@
                     </div>
                     <div class="text">
                         <p>Wat voor soort Achtbanen hebben wij aan te bieden:<p>
-                            <ul><li>Python</li><li>Joris en de draak</li><li>Pegasus</li><li>Vogel Rok</li></ul>Wij zijn natuurlijk altijd nog bezig om nieuwe en een te gekke achtbaan te inoveren, waar onze klanten "wauw" kunnen voor zeggen</p>
+                            <ul><li>Python</li><li>Joris en de draak</li><li>Pegasus</li><li>Vogel Rok</li></ul><p>Wij zijn natuurlijk altijd nog bezig om nieuwe en een te gekke achtbaan te inoveren, waar onze klanten "wauw" kunnen voor zeggen</p>
                     </div>
                 </div>
                 <div class="box">
