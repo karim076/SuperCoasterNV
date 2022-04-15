6217248f298185a79b92cc46c187af4f8a94d7c9
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-15 14:40:53 +0200

    Added new contact form (fits our style)

--- Website/contact.php
+++ Website/contact.php
@@ -9,30 +9,29 @@
     <body>
         <?php require_once 'layout/header.php' ?>
         <main>
-            <div class="wrappers">
-                <form class="form-style-9">
-                    <ul>
-                        <li>
-                            <input type="text" name="field1" class="field-style field-split align-left" placeholder="Name" />
-                            <input type="email" name="field2" class="field-style field-split align-right" placeholder="Email" />
-
-                        </li>
-                        <li>
-                            <input type="text" name="field3" class="field-style field-split align-left" placeholder="Phone" />
-                            <input type="url" name="field4" class="field-style field-split align-right" placeholder="Website" />
-                        </li>
-                        <li>
-                            <input type="text" name="field3" class="field-style field-full align-none" placeholder="Subject" />
-                        </li>
-                        <li>
-                            <textarea name="field5" class="field-style" placeholder="Message"></textarea>
-                        </li>
-                        <li>
-                            <input type="submit" value="Send Message" />
-                        </li>
-                    </ul>
-                </form>
-            </div>
+            <div class="center"><h1>Contact</h1></div>
+            <form action="#" method="post" id="contact_form">
+                <div class="name">
+                    <label for="name"></label>
+                    <input type="text" placeholder="My name is" name="name" id="name_input" required>
+                </div>
+                <div class="email">
+                    <label for="email"></label>
+                    <input type="email" placeholder="Uw email" name="email" id="email_input" required>
+                </div>
+                <div class="telephone">
+                    <label for="name"></label>
+                    <input type="text" placeholder="Uw nummer(Optioneel)" name="telephone" id="telephone_input">
+                </div>
+                <div class="message">
+                    <label for="message"></label>
+                    <textarea name="message" placeholder="Wat wilt u vertellen" id="message_input" cols="30" rows="5" required></textarea>
+                </div>
+                <div class="submit">
+                    <input type="submit" value="Verstuur bericht" id="form_button" />
+                </div>
+            </form><!-- // End form -->
+            </div><!-- // End #container -->
         </main>
 </div>
         <footer>
