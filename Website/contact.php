9fb781242cedfadcc8ae41cd5dd8d6feaaf8a646
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-15 15:03:37 +0200

    Added names and right path to backend form

--- Website/contact.php
+++ Website/contact.php
@@ -10,22 +10,22 @@
         <?php require_once 'layout/header.php' ?>
         <main>
             <div class="center"><h1>Contact</h1></div>
-            <form action="#" method="post" id="contact_form">
+            <form action="backend/contactController.php" method="POST" id="contact_form">
                 <div class="name">
                     <label for="name"></label>
-                    <input type="text" placeholder="My name is" name="name" id="name_input" required>
+                    <input type="text" placeholder="Uw naam" name="name" id="name_input" required>
                 </div>
                 <div class="email">
                     <label for="email"></label>
                     <input type="email" placeholder="Uw email" name="email" id="email_input" required>
                 </div>
-                <div class="telephone">
+                <div class="number">
                     <label for="name"></label>
-                    <input type="text" placeholder="Uw nummer(Optioneel)" name="telephone" id="telephone_input">
+                    <input type="text" placeholder="Uw nummer(Optioneel)" name="number" id="telephone_input">
                 </div>
                 <div class="message">
                     <label for="message"></label>
-                    <textarea name="message" placeholder="Wat wilt u vertellen" id="message_input" cols="30" rows="5" required></textarea>
+                    <textarea name="msg" placeholder="Wat wilt u vertellen" id="message_input" cols="30" rows="5" required></textarea>
                 </div>
                 <div class="submit">
                     <input type="submit" value="Verstuur bericht" id="form_button" />
