4f6eb4db3f09f6ed13cd493c10aa7c60ef80fde6
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-15 14:24:24 +0200

    Added box next to it img and a h1

--- Website/constructie.php
+++ Website/constructie.php
@@ -17,10 +17,17 @@
             </div>
             <div class="textCon">
                 <div class="wrapper">
-                    <p>De constructie van de achtbanen hebben een speciaal soort messing. Dit is 69% staal, 20% nikkel en 11% koper. Wij noemen dit MagnaAlloy. Door deze messing gaat de atractie nog sneller dan alle andere achtbanen en gaat de atractie tot wel 25% langer mee. Dit betekent dus dat de kosten goedkoper zijn. Door dit metaal kunnen we speciale vormen maken die we daarvoor niet konden maken.</p>
-                    <img src="img/6.jpg">
+                    <div class="texting">
+                        <div class="box">
+                            <p>De constructie van de achtbanen hebben een speciaal soort messing. Dit is 69% staal, 20% nikkel en 11% koper. Wij noemen dit MagnaAlloy. Door deze messing gaat de atractie nog sneller dan alle andere achtbanen en gaat de atractie tot wel 25% langer mee. Dit betekent dus dat de kosten goedkoper zijn. Door dit metaal kunnen we speciale vormen maken die we daarvoor niet konden maken.</p>
+                        </div>
+                        <img src="img/6.jpg">
+                    </div>
                 </div>
-                <div class="wrapper">
+                <div class="center">
+                    <h1>Video-RollerCoaster testing</h1>
+                </div>
+                    <div class="wrapper">
                     <div class="video">
                         <iframe width="1280" height="720" src="https://www.youtube.com/embed/_tL0zMdvsAI" frameborder="0"></iframe> 
                     </div>
